744a30320cd3dda0ec0bc7fb42e9bab640d7c6da
berkin <berkin@kuyabiye.com>
2009-11-05 20:40:29 +0000

    git-svn-id: http://svn.kuyabiye.com/trunk@29 56728d20-4efb-468c-88ec-50642360a07e

--- apps/frontend/modules/comment/actions/actions.class.php
+++ apps/frontend/modules/comment/actions/actions.class.php
@@ -5,7 +5,7 @@
  *
  * @package    sf_sandbox
  * @subpackage tagcomment
- * @author     Your name heree
+ * @author     Your name here
  * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
  */
 class commentActions extends sfActions
